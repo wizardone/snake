d20e7c2f29cf9e973171dc97476c7972cf24ca48
Stefan Slaveykov <wizard.oneandonly@gmail.com>
2017-09-28 12:32:51 +0300

    add express to serve the component

---
+++ server/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.port || 8000
+
+app.use(express.static(path.resolve(__dirname, '..', 'build')));
+
+// Always return the main index.html, so react-router render the route in the client
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '..', 'build', 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}!`);
+});
