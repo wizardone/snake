06ddc5e77bdaa8aa5f1f766d761b1c4e0a36f5e6
Stefan Slaveykov <wizard.oneandonly@gmail.com>
2019-11-16 16:03:42 +1300

    what is wrong??

--- src/Board.tsx
+++ src/Board.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import './Board.css';
 import { Cell, CellProps } from './Cell';
 import { coordinates } from './App';
@@ -7,36 +7,74 @@ type BoardProps = {
 	appleCoordinates: coordinates,
 	snakeCoordinates: coordinates,
 }
+
+function useInterval(callback: any, delay: number) {
+  const savedCallback: any = useRef();
+
+  // Remember the latest function.
+  useEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
+
+  // Set up the interval.
+  useEffect(() => {
+    function tick() {
+      savedCallback.current();
+    }
+    if (delay !== null) {
+      let id = setInterval(tick, delay);
+      return () => clearInterval(id);
+    }
+  }, [delay]);
+}
+
 const Board: React.FC<BoardProps> = ({appleCoordinates, snakeCoordinates}) => {
 	const [coordinates, toggleCoordinates] = useState({
 		apple: appleCoordinates,
 		snake: snakeCoordinates,
 		direction: "LEFT"
 	})
-	useEffect(() => {
-		setInterval((handler: TimerHandler) => {
+
+	useInterval(() => {
 			moveSnake()
-		}, 4000)
+	}, 500)
+
+	useEffect(() => {
 		document.addEventListener('keyup', (event: KeyboardEvent) => {
-			changeSnakeDirection()
-		})	
+			if(KEY_DIRECTIONS[event.keyCode] !== undefined) {
+				changeSnakeDirection(KEY_DIRECTIONS[event.keyCode])
+			}
+		})
 	})
 
-	const TOTAL_CELLS: number = 400;
-
-	const changeSnakeDirection = () => {
-		return coordinates.snake
+	const TOTAL_CELLS: number = 400
+	const KEY_DIRECTIONS: any = {
+		37: 'LEFT',
+		38: 'DOWN',
+		39: 'RIGHT',
+		40: 'UP'
 	}
 
-	const moveSnake = () => {
+	const changeSnakeDirection = (newDirection: string): void => {
+		toggleCoordinates({...coordinates, ...{direction: newDirection}})
+	}
+
+	const moveSnake = (): void => {
 		let snakeX: number = coordinates.snake.x
 		let snakeY:number = coordinates.snake.y
-		toggleCoordinates({...coordinates, ...{
-			snake: {
-				x: snakeX + 1,
-				y: snakeY
-			}
-		}})
+		let newCoordinates: coordinates = {x: 0, y: 0}
+
+		if(coordinates.direction === 'LEFT') {
+			newCoordinates = { x: snakeX - 1, y: snakeY }
+		} else if(coordinates.direction === 'RIGHT') {
+			newCoordinates = { x: snakeX + 1, y: snakeY }
+		} else if(coordinates.direction === 'UP') {
+			newCoordinates = { x: snakeX, y: snakeY + 1}
+		} else if(coordinates.direction === 'DOWN') {
+			newCoordinates = { x: snakeX, y: snakeY - 1}
+		}
+
+		toggleCoordinates({...coordinates, ...{snake: newCoordinates}})
 	}
 
 	const renderCells = () => {
